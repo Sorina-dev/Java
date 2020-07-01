bcc6555735bd6460183a08689bc6fadbb906940c
Sorina-dev <cristiansorina@gmail.com>
2020-07-01 14:10:04 +0300

    class 16 added

---
+++ src/com/syntax/class15/StringBufferDemo.java
@@ -0,0 +1,20 @@
+package com.syntax.class15;
+
+public class StringBufferDemo {
+	public static void main(String[] args) {
+		
+	
+
+	StringBuffer strBuffer = new StringBuffer("Hello");//is a mutable object
+	
+	strBuffer.append("friends"); //when we perform the operation it happens on same Object and No new Objects gets created
+	
+	System.out.println(strBuffer); //Hello friends
+	
+	String str2 = "Hello";
+	str2.concat("firends");
+	
+	System.out.println(str2); //Hello
+	
+	}
+}
