2420fe1dffcf862143d1bc3ea0fa113253416549
Sorina-dev <cristiansorina@gmail.com>
2020-07-27 11:05:38 +0300

    arrayList

---
+++ src/com/syntax/class29/Task.java
@@ -0,0 +1,34 @@
+package com.syntax.class29;
+
+import java.util.ArrayList;
+
+/*
+ * Create an ArrayList that will store 5 names into it. 
+Find out whether the given ArrayList is empty or not? 
+Check whether the specific name is present in an ArrayList or not?
+Find the size of your arrayList and print all values from that
+
+ */
+public class Task {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> names = new ArrayList<>(5);
+		names.add("Sorina");
+		names.add("Victor");
+		names.add("Adrian");
+		names.add("Evelina");
+		names.add("Natalia");
+		
+		boolean empty = names.isEmpty();
+		System.out.println(empty);
+		
+		boolean contains = names.contains("Sorina");
+		System.out.println(contains);
+		
+		int size = names.size();
+		System.out.println(size);
+		
+		System.out.println(names);
+	}
+}
